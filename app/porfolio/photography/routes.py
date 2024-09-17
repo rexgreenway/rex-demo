23fda7b8ef211e8f0fe4b3ac1bdee45bd1ccfc73
Rex Greenway <rex.greenway@neo4j.com>
2024-09-17 15:06:43 +0100

    first google storage attempts

--- app/porfolio/photography/routes.py
+++ app/porfolio/photography/routes.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 
+from .utils import download_blob_into_memory, list_blobs
+
 photography_router = APIRouter()
 
 
 @photography_router.get("/photography")
 async def photography():
-    return {"message": "Photography..."}
+    blobs = list_blobs()
+
+    download_blob_into_memory()
+
+    return {"message": "Photography...", "blobs": blobs}
