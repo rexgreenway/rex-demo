1d5185676495ddbb91c60dc772579277d4242a8a
Rex Greenway <rex.greenway@neo4j.com>
2024-09-17 13:32:27 +0100

    add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .polylatlib import pl_router
 
 # Establish Core API
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "https://rexgreenway.github.io/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register Sub-routers
 app.include_router(pl_router)
