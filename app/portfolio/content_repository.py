793419f4a96e18979a225294c5fae430a64515b5
Rex <rexgreenway@proton.me>
2024-10-18 22:47:42 +0100

    proper credentials fetching in cloud run

--- app/portfolio/content_repository.py
+++ app/portfolio/content_repository.py
@@ -2,7 +2,10 @@ import datetime
 from enum import Enum
 
 from cachetools import TTLCache, cached
+from google.auth import default
+from google.auth.transport import requests
 from google.cloud import storage
+from google.cloud.storage import Blob
 from pydantic import BaseModel
 
 FILM_PHOTOGRAPHY_BUCKET = "rex-photography-portfolio"
@@ -31,7 +34,25 @@ class ContentRepository:
     """Repository for fetching photographs from"""
 
     def __init__(self):
-        self.client = storage.Client(project="ultra-path-385820")
+        try:
+            self.credentials, self.project_id = default()
+            self.client = storage.Client(self.project_id)
+
+        except Exception as e:
+            raise PermissionError("failed to create Google Storage Client") from e
+
+    def _generate_signed_url(self, blob: Blob) -> str:
+        """https://stackoverflow.com/questions/73918501/why-my-cloud-run-instance-is-using-the-default-service-account-instead-of-my-ded"""
+        # Perform a refresh token with a request to generate a token (Else, it's None)
+        r = requests.Request()
+        self.credentials.refresh(r)
+
+        return blob.generate_signed_url(
+            service_account_email=self.credentials.service_account_email,
+            access_token=self.credentials.token,
+            method="GET",
+            expiration=datetime.timedelta(minutes=TTL_MINUTES),
+        )
 
     @cached(cache=TTLCache(maxsize=1024, ttl=datetime.timedelta(minutes=TTL_MINUTES), timer=datetime.datetime.now))
     def get_album(self, album_name: str, size: Size = Size.SMALL) -> Album:
@@ -48,11 +69,7 @@ class ContentRepository:
             images.append(
                 Image(
                     path=f"{FILM_PHOTOGRAPHY_BUCKET}/{blob.name}",
-                    url=blob.generate_signed_url(
-                        version="v4",
-                        expiration=datetime.timedelta(minutes=TTL_MINUTES),
-                        method="GET",
-                    ),
+                    url=self._generate_signed_url(blob),
                 )
             )
 
@@ -68,9 +85,5 @@ class ContentRepository:
         blob = bucket.get_blob(f"{album_name}/{size.value}/{image_name}")
         return Image(
             path=f"{FILM_PHOTOGRAPHY_BUCKET}/{blob.name}",
-            url=blob.generate_signed_url(
-                version="v4",
-                expiration=datetime.timedelta(minutes=TTL_MINUTES),
-                method="GET",
-            ),
+            url=self._generate_signed_url(blob),
         )
